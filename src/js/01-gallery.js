46d1f9884a2673e48e2f7458fc6ca443c5e7b790
AllaKaplia <118439299+AllaKaplia@users.noreply.github.com>
2023-04-10 13:30:47 +0300

    first commit update

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,8 +1,10 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+console.log(galleryItems);
 // Описаний в документації
 import SimpleLightbox from "simplelightbox";
+console.log(SimpleLightbox);
 // Додатковий імпорт стилів
 import "simplelightbox/dist/simple-lightbox.min.css";
 
