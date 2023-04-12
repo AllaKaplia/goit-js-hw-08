ab976ca706b9c9d3d79b647e9e01888db40b48f2
AllaKaplia <118439299+AllaKaplia@users.noreply.github.com>
2023-04-12 23:12:59 +0300

    Settings video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
+import  throttledFun from 'lodash.throttle';
 
+const iframeEl = document.querySelector('#vimeo-player');
+const player = new Player(iframeEl);
+
+player.on("timeupdate", throttledFun(onVideoPlay, 1000));
+
+function onVideoPlay({ seconds }) {
+    localStorage.setItem('videoplayer-current-time', JSON.stringify(Math.round(seconds)));
+    console.log('played the video!', Math.round(seconds));
+}
+
+player.setCurrentTime(
+    JSON.parse(localStorage.getItem('videoplayer-current-time'))
+);
