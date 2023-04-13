00a3bc3669c043866e345ce733e2b89293dc0ab4
AllaKaplia <118439299+AllaKaplia@users.noreply.github.com>
2023-04-14 00:51:55 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,16 +1,18 @@
 import Player from '@vimeo/player';
 import  throttledFun from 'lodash.throttle';
 
+const VIDEO_TIME_KEY = 'videoplayer-current-time';
+
 const iframeEl = document.querySelector('#vimeo-player');
 const player = new Player(iframeEl);
 
 player.on("timeupdate", throttledFun(onVideoPlay, 1000));
 
 function onVideoPlay({ seconds }) {
-    localStorage.setItem('videoplayer-current-time', JSON.stringify(Math.round(seconds)));
+    localStorage.setItem(VIDEO_TIME_KEY, JSON.stringify(Math.round(seconds)));
     console.log('Played the video!', Math.round(seconds));
 }
 
 player.setCurrentTime(
-    JSON.parse(localStorage.getItem('videoplayer-current-time'))
+    JSON.parse(localStorage.getItem('VIDEO_TIME_KEY'))
 );
